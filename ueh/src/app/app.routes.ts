073b4f33421392a7140ccd6c99221a66570ee16c
Ronja Fuchs <ronja.fuchs@outlook.de>
2024-03-05 16:11:33 +0100

    add routing

--- ueh/src/app/app.routes.ts
+++ ueh/src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TeamsComponent } from './teams/teams.component';
+import { CommunityComponent } from './community/community.component';
+import { ImpressumComponent } from './impressum/impressum.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'teams', component: TeamsComponent },
+    { path: 'community', component: CommunityComponent },
+    { path: 'impressum', component: ImpressumComponent },
+
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
---
+++ ueh/src/app/teams/teams.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-teams',
+  standalone: true,
+  imports: [],
+  templateUrl: './teams.component.html',
+  styleUrl: './teams.component.scss'
+})
+export class TeamsComponent {
+
+}
